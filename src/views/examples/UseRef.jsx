44d194aa34e8aba1297212f99d807f058c9008e8
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-02-23 17:55:29 -0300

    feat(UseRef): exercise 2 completed

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -2,13 +2,29 @@ import React, { useState, useRef, useEffect } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 import SectionTitle from "../../components/layout/SectionTitle";
 
+const merge = (string1, string2) => {
+  return [...string1]
+    .map((letter, index) => `${letter}${string2[index] || ""}`)
+    .join("");
+};
+
 const UseRef = (props) => {
   const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
+
   const count = useRef(0);
+  const myInput1 = useRef(null);
+  const myInput2 = useRef(null);
 
   useEffect(() => {
     count.current = count.current + 1;
+    myInput2.current.focus();
   }, [value1]);
+
+  useEffect(() => {
+    count.current++;
+    myInput1.current.focus();
+  }, [value2]);
 
   return (
     <div className="UseRef">
@@ -21,7 +37,7 @@ const UseRef = (props) => {
       <div className="center">
         <div>
           <span className="text">Valor: </span>
-          <span className="text">{value1} [</span>
+          <span className="text">{merge(value1, value2)} [</span>
           <span className="text red">{count.current}</span>
           <span className="text">]</span>
         </div>
@@ -29,7 +45,19 @@ const UseRef = (props) => {
           type="text"
           className="input"
           value={value1}
+          ref={myInput1}
           onChange={(e) => setValue1(e.target.value)}
+        />
+      </div>
+
+      <SectionTitle title="Exercício #02" />
+      <div className="center">
+        <input
+          type="text"
+          className="input"
+          value={value2}
+          ref={myInput2}
+          onChange={(e) => setValue2(e.target.value)}
         />
       </div>
     </div>
