0f8ee5bfd13dceafa2b3bab5b6a37db6ab7768ff
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-02-25 13:37:20 -0300

    feat(UseContext): exercise 1 completed

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -6,18 +6,21 @@ import Menu from "../components/layout/Menu";
 import Content from "../components/layout/Content";
 
 import DataContext, { data } from "../data/DataContext";
+import Store from "../data/Store";
 
 const App = (props) => {
   const [state, setState] = useState(data);
 
   return (
     <div className="App">
-      <DataContext.Provider value={{ state, setState }}>
-        <Router>
-          <Menu />
-          <Content />
-        </Router>
-      </DataContext.Provider>
+      <Store>
+        <DataContext.Provider value={{ state, setState }}>
+          <Router>
+            <Menu />
+            <Content />
+          </Router>
+        </DataContext.Provider>
+      </Store>
     </div>
   );
 };
--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,7 +1,10 @@
 import React, { useContext } from "react";
+
 import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 import DataContext from "../../data/DataContext";
+import { AppContext } from "../../data/Store";
 
 const UseContext = (props) => {
   const context = useContext(DataContext);
@@ -13,12 +16,16 @@ const UseContext = (props) => {
     });
   }
 
+  const { number, text, setNumber } = useContext(AppContext);
+
   return (
     <div className="UseContext">
       <PageTitle
         title="Hook UseContext"
         subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
       />
+
+      <SectionTitle title="Exercício #01" />
       <div className="center">
         <span className="text">{context.state.number}</span>
         <span className="text">{context.state.text}</span>
@@ -28,6 +35,19 @@ const UseContext = (props) => {
             -1
           </button>
           <button className="btn" onClick={() => addNumber(1)}>
+            +1
+          </button>
+        </div>
+      </div>
+      <SectionTitle title="Exercício #02" />
+      <div className="center">
+        <span className="text">{text}</span>
+        <span className="text">{number}</span>
+        <div>
+          <button className="btn" onClick={(e) => setNumber(number - 1)}>
+            -1
+          </button>
+          <button className="btn" onClick={(e) => setNumber(number + 1)}>
             +1
           </button>
         </div>
